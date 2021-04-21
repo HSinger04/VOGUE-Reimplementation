7447fab0d536b0b6ae9a7b364008e0468246686c
HSinger04 <hermannsinger@gmx.net>
2021-04-21 14:13:36 +0200

    Update modulated_conv2d.py

--- cryu854/modules/layers/modulated_conv2d.py
+++ cryu854/modules/layers/modulated_conv2d.py
@@ -49,7 +49,7 @@ class modulated_conv2d(tf.keras.layers.Layer):
         self.p = self.add_weight(name='p',
                                  shape=(x_shape[-1]),
                                  dtype=tf.float32,
-                                 initializer=tf.random_normal_initializer(),
+                                 initializer=tf.zeros_initializer(),
                                  trainable=True)
         
     def call(self, inputs, training=None):

c01ad578abbcd5d3404212d12315c06c45d2df11
HSinger04 <hermannsinger@gmx.net>
2021-04-22 15:14:59 +0200

    Try out modulated_conv2d's try_on without using self.p

--- cryu854/modules/layers/modulated_conv2d.py
+++ cryu854/modules/layers/modulated_conv2d.py
@@ -105,17 +105,20 @@ class modulated_conv2d(tf.keras.layers.Layer):
     def try_on(self, inputs, training=None):
         x, w_latents_p, w_latents_g = inputs
         
+        # TODO: remove
+        s = self.fully_connected(w_latents_p) + 1
+        
         # TODO: Probably need to batch like w_latents_p
         # get Q
-        q = tf.keras.activations.sigmoid(self.p)
-        Q = tf.linalg.diag(q)
+#        q = tf.keras.activations.sigmoid(self.p)
+#        Q = tf.linalg.diag(q)
         
         # get sigmas
-        sigma_p = self.fully_connected(w_latents_p) + 1
-        sigma_g = self.fully_connected(w_latents_g) + 1
+#        sigma_p = self.fully_connected(w_latents_p) + 1
+#        sigma_g = self.fully_connected(w_latents_g) + 1
         
         # Linear interpolation
-        s = sigma_p + tf.linalg.matmul(Q, sigma_g - sigma_p)
+#        s = sigma_p + tf.linalg.matmul(Q, sigma_g - sigma_p)
         
         # Transform to channel first.
         x = tf.transpose(x, [0, 3, 1, 2])
